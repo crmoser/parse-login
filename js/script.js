e700519eb8ac4d1c524da4f2df78862075db4ea5
crmoser <collin.moser@gmail.com>
2015-01-08 12:56:57 -0500

    cleanup and iterative loops for user info

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 Parse.initialize("MhsTx95fClaI5CSB7pPqBoC2BPJvOwyIvRQMTCQS", "yZZMGCjCHlIryXe7cRFqkjrZFOTxf4j587H0gAzw");
 
 window.fbAsyncInit = function() {
-  
+
 	Parse.FacebookUtils.init({
 		appId      : '642657672506723',
 		status     : true,  
@@ -10,42 +10,87 @@ window.fbAsyncInit = function() {
 		version    : 'v2.2'
 	});
 
-  function userInfo() {
-    var user = Parse.User.current();
+  function login() {
+    var username = $('#username').val(),
+        password = $('#password').val();
 
-    if (Parse.FacebookUtils.isLinked) {
-        $("#userinfo").html(
-            '<img src=' + user.attributes.profilepic + '/>' + '<br />' +
-            'Name: ' + user.attributes.name + '<br />' +
-            'Username: ' + user.attributes.username + '<br />' +
-            'Email: ' + user.attributes.email + '<br />' +
-            'Location: ' + user.attributes.location
-        );
-
-      $("#connect").text("Yay, your accounts are linked.");
-      $("#signout, #userinfo").show();
-
-    } else if (user) {
-      $("#logout, #facebook").css('display', 'block');
-      $('#username, #password, #note, #mainlogin h1').hide();
-
-      $("#userinfo").html(
-          'Username: ' + user.attributes.username + '<br />' +
-          'Email: ' + user.attributes.email + '<br />' 
-      ).show();
-
-    } else {
-      $("#connect").text("Connect with Facebook");
-      $("#signout, #userinfo").hide();
-    }
+    Parse.User.logIn(username, password, {
+      success: function(user) {
+        updateUser();
+      },
+      error: function(user, error) {
+        alert(error.message);
+      }
+    });
   }
 
-  function fbLink() {
+  function logout() {
+    Parse.User.logOut();
+    $("#facebook").hide();
+    $('#username, #password, #forgot, #mainlogin h1').show();
+    $("#userinfo").html('');
+    $("#logout").attr('id', 'login').text('Login');
+  }
+
+  function updateUser() {
+    var user = Parse.User.current();
+    if (user) {
+      var obj = user.attributes;
+      Object.keys(obj).forEach(function (key) {
+        $("#userinfo").append('<p>' + key + ' : ' + obj[key] + '</p>').show();
+      });
+      $("#facebook").show();
+      $('#username, #password, #forgot, #mainlogin h1').hide();
+      $("#login").attr('id', 'logout').text('Logout');
+    }
+
+    // if (Parse.FacebookUtils.isLinked) {
+    //     $("#userinfo").html(
+    //         '<img src=' + user.attributes.profilepic + '/>' + '<br />' +
+    //         'Name: ' + user.attributes.name + '<br />' +
+    //         'Username: ' + user.attributes.username + '<br />' +
+    //         'Email: ' + user.attributes.email + '<br />' +
+    //         'Location: ' + user.attributes.location
+    //     );
+
+    //   $("#link").text("Yay, your accounts are linked.");
+    //   $("#signout, #userinfo").show();
+
+    // } else if (user) {
+    //   $("#logout, #facebook").css('display', 'block');
+    //   $('#username, #password, #forgot, #mainlogin h1').hide();
+
+    //   $("#userinfo").html(
+    //       'Username: ' + user.attributes.username + '<br />' +
+    //       'Email: ' + user.attributes.email + '<br />' 
+    //   ).show();
+
+    // } else {
+    //   $("#link").text("Connect with Facebook");
+    //   $("#signout, #userinfo").hide();
+    // }
+  }
+
+  function resetPassword(email) {
+    Parse.User.requestPasswordReset(email, {
+      success: function() {
+        alert("Check your email to reset your password.");
+        $('#username, #password, #forgot, #mainlogin h1').show();
+        $('#resetform').hide(); 
+      },
+      error: function(error) {
+        // Show the error message somewhere
+        alert(error.message);
+      }
+    });
+  }
+
+  function linkFB() {
     // Make a new Parse user, 'sign in' with FB, and set their info in the Parse.user object.
    //  var user = new Parse.User();
    //  Parse.FacebookUtils.logIn('email, user_location', {
 			// success: function(user) {
-   //      userInfo();
+   //      updateUser();
    //    },
    //    error: function(user, error) {
    //      alert(error.message);
@@ -91,77 +136,41 @@ window.fbAsyncInit = function() {
 
   }
 
-  function fbUnlink() {
+  function unlinkFB() {
     var user = Parse.User.current();
     Parse.FacebookUtils.unlink(user, {
       success: function(user) {
         alert("The user is no longer associated with their Facebook account.");
-        $("#connect").text("Connect with Facebook");
+        $("#link").text("Link your Facebook account");
         $("#signout").hide();
       }
     });
   }
-
-  function parseSignIn() {
-    var username = $('#username').val(),
-        password = $('#password').val();
-
-    Parse.User.logIn(username, password, {
-      success: function(user) {
-        $("#logout, #facebook").css('display', 'block');
-        $('#username, #password, #note, #mainlogin h1').hide();
-        userInfo();
-      },
-      error: function(user, error) {
-        alert(error.message);
-      }
-    });
-  }
-
-  function parseSignOut() {
-    Parse.User.logOut();
-    $("#logout").hide();
-    $('#username, #password, #note, #mainlogin h1').show();
-  }
-
-  function resetPassword(email) {
-    Parse.User.requestPasswordReset(email, {
-      success: function() {
-        alert("Check your email to reset your password.");
-        $('#username, #password, #note, #mainlogin h1').show();
-        $('#resetform').hide(); 
-      },
-      error: function(error) {
-        // Show the error message somewhere
-        alert(error.message);
-      }
-    });
-  }
   
-  userInfo();
+  updateUser();
 
   //Event Handlers
-  $("#connect").click(function() {
-      fbLink();
+  $("#link").click(function() {
+      linkFB();
   });
 
   $("#signout").click(function() {
-    fbUnlink();
-    userInfo();
+    unlinkFB();
+    updateUser();
   });
 
-  $("#mainlogin button").click(function (e) {
-    parseSignIn();
+  $("button").on('click', function (e) {
+    if ($(this).attr('id') === 'login') {
+      login();
+    } else {
+      logout();
+    }
     e.preventDefault();
   });
 
-  $("#logout").click(function() {
-    parseSignOut();
-  });
-
-  $("#note").click(function () {
+  $("#forgot").click(function () {
     $(this).hide();
-    $("#username, #password, #note").hide();
+    $("#username, #password, #forgot").hide();
     $("#resetform").show();
   });
 
